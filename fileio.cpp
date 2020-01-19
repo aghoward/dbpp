aa17a9a6884a7148ac8c08e217580fd38ac47590
Austin Howard <austinghoward@gmail.com>
2020-01-19 10:47:55 -0500

    Exclude blank entries
    Blank entries caused a bug whereby recursive searches could continue
    forever because the executor always adds a "/" between the url and the
    entry. This excludes blank entries from the word list because they don't
    make sense anyway

--- fileio.cpp
+++ fileio.cpp
@@ -6,12 +6,15 @@
 
 std::vector<std::string> get_word_list(const std::string& file)
 {
+    using namespace std::string_literals;
+
     auto words = std::vector<std::string>();
     std::ifstream fs(file);
     std::string str;
 
     while (std::getline(fs, str))
-        words.push_back(str);
+        if (str != ""s)
+            words.push_back(str);
     
     return words;
 }
