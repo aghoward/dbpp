846004f266758a8bfc2b48c3a15e5d50fe81dec4
Austin Howard <austinghoward@protonmail.com>
2021-09-01 17:28:37 -0400

    Simplify String Formatting

--- support/template_formatting.cpp
+++ support/template_formatting.cpp
@@ -9,16 +9,9 @@ namespace impl
     std::string format_single(const std::string& _template, const std::pair<std::string, std::string>& replacement)
     {
         auto result = _template;
-        auto index = result.find(replacement.first);
-        while (index != std::string::npos)
-        {
-            auto replacement_end_index = index + replacement.first.size() - 1;
-            auto end_index = result.size() - 1;
-            auto end = result.substr(replacement_end_index + 1, end_index - replacement_end_index);
-            auto begin = result.substr(0, index);
-            result = begin + replacement.second + end;
-            index = result.find(replacement.first);
-        }
+        auto index = 0ul;
+        while ((index = result.find(replacement.first)) != std::string::npos)
+            result.replace(index, replacement.first.size(), replacement.second);
 
         return result;
     }
